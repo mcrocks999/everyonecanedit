fa390cd0b71cc4558a46540d1be26184ef8581f5
mcrocks999 <paulmakles@gmail.com>
2017-02-25 13:43:35 +0000

    Removed link modifier

--- default.php
+++ default.php
@@ -1,7 +1,6 @@
 <html>
 <head>
 <title>Everyonecanedit.tk</title>
-<script type="text/javascript" src="http://coinurl.com/script/jquery-latest.min.js"></script>
 <style>
 table td, table td * {
     vertical-align: top;
@@ -17,50 +16,6 @@ word-break: break-all;
 white-space: normal;
 }
 </style>
-<script type="text/javascript">
-$(function() {
-    var include = Array();
-    //Leave empty to convert all links on the page or specify keywords
-    //which URL must contain to be processed
-    
-    var exclude = Array("everyonecanedit.tk/edit.php");
-    //Specify keywords which URL must not contain to be processed
-    
-    var id = "172053fe29b7083d6f893f15ac029c55";
-    var redirect = "http://cur.lv/redirect.php?id=" + id + "&url=";
-    var links = $("a[href^='http']");
-    
-    for(var i = 0; i < links.length; i++) {
-        var url = $(links[i]).attr("href");
-        
-        var deny = false;
-        for(var j = 0; j < exclude.length; j++) {
-            if(url.indexOf(exclude[j]) != -1) {
-                deny = true;
-                break;
-            }
-        }
-        if(deny) {
-            continue;
-        }
-        
-        if(include.length > 0) {
-            var allow = false;
-            for(var j = 0; j < include.length; j++) {
-                if(url.indexOf(include[j]) != -1) {
-                    allow = true;
-                    break;
-                }
-            }
-            if(!allow) {
-                continue;
-            }
-        }
-        
-        $(links[i]).attr("href",  redirect + encodeURIComponent(url));
-    }
-});
-</script>
 </head>
 <body>
 <table width=100%>
