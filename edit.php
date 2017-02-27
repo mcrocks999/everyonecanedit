6f054bc7f4a6de43ca1ef06355ba9de60bc65e76
mcrocks999 <mcrocks999@users.noreply.github.com>
2017-02-27 12:42:08 +0000

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,34 +1,30 @@
 <?php
-
-// configuration
-$url = 'http://everyonecanedit.tk';
-$file = './content.html';
-
-// check if form has been submitted
-if (isset($_POST['text']))
-{
-    // save the text contents
-    file_put_contents($file, $_POST['text']);
-
-    // redirect to form again
-    header(sprintf('Location: %s', $url));
-    printf('<a href="%s">Moved</a>.', htmlspecialchars($url));
-    exit();
-}
-
-// read the textfile
-$text = file_get_contents($file);
-
+	$url = 'default.php';
+	$file = './content.html';
+	if (isset($_POST['text']))
+	{
+	file_put_contents($file, $_POST['text']);
+	header(sprintf('Location: %s', $url));
+	printf('<a href="%s">Moved</a>.', htmlspecialchars($url));
+	exit();
+	}
+	$text = file_get_contents($file);
 ?>
-<style>
-textarea {
-width: 100%;
-height: 90%;
-}
-</style>
-<!-- HTML form -->
-<form action="" method="post">
-<textarea name="text" 3><?php echo stripslashes($text)?></textarea>
-<input type="submit" value="Save"/>&nbsp;
-<a href=".">Cancel</a>
-</form>
+<html>
+	<head>
+		<title>Edit</title>
+		<style>
+			textarea {
+				width: 100%;
+				height: 90%;
+			}
+		</style>
+	</head>
+	<body>
+		<form action="" method="post">
+			<textarea name="text" 3><?php echo stripslashes($text)?></textarea>
+			<input type="submit" value="Save"/>&nbsp;
+			<a href=".">Cancel</a>
+		</form>
+	</body>
+</html>
